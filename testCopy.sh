e609ae0508229c34a30ba4e87b4c1638fc636c26
JohnGoku <Sporbert.Falk@gmail.com>
2022-03-24 09:48:01 +0100

    ToDo: Change working directory of Python casts

--- testCopy.sh
+++ testCopy.sh
@@ -1,10 +1,19 @@
-#!/bin/bash
+#!/bin/bash                                                                                                                                                                                                                                                                                                                                                                                                                                                                               #Skript fuehrt apiTest.py aus und kopiert anschliessend die notwendigen Dateien fuer die Webseite an den passenden Ort                                                                                                                                                                                                                                                                                                                                                                    function apis {                                                                                                                                                                                                                                  python3 - <<END                                                                                                                                                                                                                          from python.apiTests import checkIfAvailable                                                                                                                                                                                                 checkIfAvailable()                                                                                                                                                                                                                           END                                                                                                                                                                                                                                          }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         working= apis                                                                                                                                                                                                                                echo $working                                                                                                                                                                                                                                echo apis                                                                                                                                                                                                                                    if($working)                                                                                                                                                                                                                                         then                                                                                                                                                                                                                                         echo "test" >> ../../test.txt                                                                                                                                                                                                        #       cp -f *.html *.pdf /var/www/html                                                                                                                                                                                                     fi  #!/bin/bash
 
-#Skript führt apiTest.py aus und kopiert anschließend die notwendigen Dateien für die Webseite an den passenden Ort
+#Skript fuehrt apiTest.py aus und kopiert anschliessend die notwendigen Dateien fuer die Webseite an den passenden Ort
 
+function apis {
+    python3 - <<END
+from python.apiTests import checkIfAvailable
+checkIfAvailable()
+END
+}
 
-if(python apiTest.py)
-	then
-	echo "test" >> ../../test.txt
-#	cp -f *.html *.pdf /var/www/html
+working= apis
+echo $working
+echo apis
+if($working)
+        then
+        echo "test" >> ../../test.txt
+#       cp -f *.html *.pdf /var/www/html
 fi
