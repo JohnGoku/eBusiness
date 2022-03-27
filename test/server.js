98471ef42969bb8a335239eb46b97c9f06d0ae89
JohnGoku <Sporbert.Falk@gmail.com>
2022-03-27 18:42:41 +0200

    added check for containing fraport to tests

--- test/server.js
+++ test/server.js
@@ -5,13 +5,18 @@ var request = require("request");
 
 describe("Communication with APIS", function () {
 
-    describe("running symbol search", function () {
+    describe("running symbol search with input FRA", function () {
         var url = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=FRA&apikey=QQQLXZK291FP2Z1O"
         it("returns status 200", function () {
             request(url, function (error, response, body) {
                 expect(response.statusCode).to.equal(200);
-            }); });
-        it("returns the Blackrock", function () { });
+            });
+        });
+        it("Contains Fraport", function () {
+            request(url, function (error, response, body) {
+                body.should.include("Fraport");;
+            });
+        });
     });
 
     describe("running Time Series Daily", function () {
