9d48e461aa2f233d37bf10a1128e63ea5fa599c1
JohnGoku <Sporbert.Falk@gmail.com>
2022-03-27 18:30:32 +0200

    added response code check in test/server.js

--- test/server.js
+++ test/server.js
@@ -7,13 +7,19 @@ describe("Communication with APIS", function () {
 
     describe("running symbol search", function () {
         var url = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=FRA&apikey=QQQLXZK291FP2Z1O"
-        it("returns status 200", function () { });
+        it("returns status 200", function () {
+            request(url, function (error, response, body) {
+                expect(response.statusCode).to.equal(300);
+            }); });
         it("returns the Blackrock", function () { });
     });
 
     describe("running Time Series Daily", function () {
         var url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=FRA&apikey=QQQLXZK291FP2Z1O"
-        it("returns status 200", function () { });
+        it("returns status 200", function () {
+            request(url, function (error, response, body) {
+                expect(response.statusCode).to.equal(300);
+            }); });
         it("returns the TSD-JSON", function () { });
     });
 
